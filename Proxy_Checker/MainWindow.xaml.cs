c070a2ef5a8e842e444d38328f4d0f03d98a6a53
Hamza Anis <hamzaanis9514@gmail.com>
2017-09-23 07:38:15 +0500

    Before data table.

--- Proxy_Checker/MainWindow.xaml.cs
+++ Proxy_Checker/MainWindow.xaml.cs
@@ -1,24 +1,15 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Diagnostics;
-using System.Linq;
-using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+using MaterialDesignThemes.Wpf;
 
 namespace Proxy_Checker
 {
     /// <summary>
-    /// Interaction logic for MainWindow.xaml
+    ///     Interaction logic for MainWindow.xaml
     /// </summary>
     public partial class MainWindow : Window
     {
@@ -29,6 +20,22 @@ namespace Proxy_Checker
 
         private void Button_Click(object sender, RoutedEventArgs e)
         {
+        }
+
+        private void Sample1_DialogHost_OnDialogClosing(object sender, DialogClosingEventArgs dialogClosingEventArgs)
+        {
+            Console.WriteLine("SAMPLE 1: Closing dialog with parameter: " + (dialogClosingEventArgs.Parameter ?? ""));
+
+            //you can cancel the dialog close:
+            //eventArgs.Cancel();
+
+            if (!Equals(dialogClosingEventArgs.Parameter, true)) return;
+
+            if (!string.IsNullOrWhiteSpace(FruitTextBox.Text))
+//                FruitListBox.Items.Add(string.Format("{0}   |   {1}", FruitTextBox.Text, "Hamza Ains"));
+//            FruitListBox.Items.Add("Column3Text").SubItems.AddRange(new string[] { "col1;row3", "col2;row3", "col3;row3" });
+
+
         }
 
         private void Flipper_OnIsFlippedChanged(object sender, RoutedPropertyChangedEventArgs<bool> e)
@@ -45,7 +52,6 @@ namespace Proxy_Checker
 
         private void Button_Click_1(object sender, RoutedEventArgs e)
         {
-
         }
     }
 }
